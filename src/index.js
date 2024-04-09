f4e8d8d8583b75aa93476f76ecb2efc1f6479ba3
Carlos Fernandez <dominguezalacid@gmail.com>
2024-04-09 21:29:01 +0200

    actualizar

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import path from "path";
 import dotenv from "dotenv";
 import cors from "cors";
 import routes from "./routes/index.js";
-import multerMiddleware from "./middlewares/multer.middleware.js";
 import "./service/mongoDB.js";
 
 if (process.env.NODE_ENV !== 'production') {
@@ -24,20 +23,22 @@ app.set("port", process.env.PORT || 3000);
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 //Configurar rutas de archivos estaticos (imagenes)
+app.use(express.static('./public'));
+app.use(express.static(path.join(__dirname, "../public/data")));
 app.use('/vtes-backend/uploads', express.static(path.join(__dirname, "uploads")));
 app.use('/vtes-backend/uploads/avatars', express.static(path.join(__dirname, "uploads/avatars")));
 app.use('/vtes-backend/uploads/customCards', express.static(path.join(__dirname, "uploads/customCards")));
 
+
 //ROUTES
 app.use(routes);
-
-app.use(cors({
-    origin: '*'
-}));
-
 
 //MONGODB CONEXION
 app.listen(app.get("port"), () => {
@@ -45,8 +46,17 @@ app.listen(app.get("port"), () => {
     console.log("En el modo: ", process.env.NODE_ENV);
 })
 
-app.get('/data', (req, res) => {
-    res.send('Datos desde backend');
+app.get('/', (req, res) => {
+    res.send('Backend server running...')
 })
+
+app.get('/vtes.json', (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/data/vtes.json"));
+})
+
+//404
+app.use((req, res, next) => {
+    res.status(404).send('404 Not found')
+});
 
 export default app;
