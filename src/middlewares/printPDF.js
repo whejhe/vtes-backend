b5ff98acb8f9a1dd9137908f01c58c82d9ad9df9
whejhe <dominguezalacid@gmail.com>
2024-04-30 09:09:05 +0200

    actualizar

--- src/middlewares/printPDF.js
+++ src/middlewares/printPDF.js
@@ -3,7 +3,9 @@ import PDFDocument from 'pdfkit';
 import blobStream from 'blob-stream';
 import Cards from "../models/cards.model.js";
 import Deck from "../models/deck.model.js";
-
+import path from 'path';
+import fs from 'fs';
+import got from 'got';
 
 export const generateDeckPDF = async (req, res) => {
     try {
@@ -14,28 +16,48 @@ export const generateDeckPDF = async (req, res) => {
             return res.status(404).json({ error: 'Mazo no encontrado' });
         }
 
+        const downloadAndSaveImage = async (url) => {
+            const __dirname = path.resolve();
+
+            const fileName = path.basename(url);
+            console.log(__dirname, 'dirname');
+            const filePath = `${__dirname}/uploads/vtesCards/${fileName}`;
+
+            // Verificar si el archivo ya existe
+            if (fs.existsSync(filePath)) {
+                return filePath;
+            }
+
+            // Si el archivo no existe, lo descarga y lo guarda
+            const response = await got(url, { responseType: 'buffer' });
+            fs.writeFileSync(filePath, response.body);
+            return filePath;
+        };
+
         const cards = await Promise.all(deck.crypt.map(async (card) => {
             const cardData = await Cards.findById(card._id);
-            if (cardData && cardData.imageUrl) { 
+            if (cardData && cardData.url) {
+                const imageUrl = await downloadAndSaveImage(cardData.url, cardData.name);
                 return {
                     name: cardData.name,
-                    imageUrl: cardData.imageUrl,
+                    imageUrl: imageUrl,
                 };
             }
         }));
 
         const libraryCards = await Promise.all(deck.library.map(async (card) => {
             const cardData = await Cards.findById(card._id);
-            if (cardData && cardData.imageUrl) { 
+            if (cardData && cardData.url) {
+                const imageUrl = await downloadAndSaveImage(cardData.url, cardData.name);
                 return {
                     name: cardData.name,
-                    imageUrl: cardData.imageUrl,
+                    imageUrl: imageUrl,
                 };
             }
         }));
 
         const doc = new PDFDocument({ size: 'A4', margin: 50 });
-        const stream = doc.pipe(blobStream());
+        const buffers = [];
 
         // Agrega el título del mazo
         doc.fontSize(18).text(`Mazo: ${deck.name}`, { align: 'center' });
@@ -62,17 +84,17 @@ export const generateDeckPDF = async (req, res) => {
             doc.moveDown(3);
         }
 
-        // Finaliza el documento PDF
-        doc.end();
-
-        stream.on('finish', () => {
-            const pdfBuffer = stream.toBuffer();
+        doc.on('data', buffers.push.bind(buffers));
+        doc.on('end', () => {
+            const pdfData = Buffer.concat(buffers);
             res.setHeader('Content-Type', 'application/pdf');
             res.setHeader('Content-Disposition', `attachment; filename="${deck.name}.pdf"`);
-            res.send(pdfBuffer);
+            res.end(pdfData);
         });
+
+        doc.end();
     } catch (error) {
-        console.log('Error: ',error);
+        console.log('Error: ', error);
         return res.status(400).json({ error: error.message });
     }
 };
