babffd0c4e3bbae0473869702d43f4b3e8107a74
whejhe <dominguezalacid@gmail.com>
2024-05-28 14:54:33 +0200

    actualizar

--- src/middlewares/deckCheck.js
+++ src/middlewares/deckCheck.js
@@ -5,7 +5,7 @@ export const deckCheck = async (req, res, next) => {
     try {
         const { id } = req.params;
         const { user } = req
-        console.log(user, 'user')
+        // console.log(user, 'user')
         // const userId = await Deck.findById(user._id);
         // console.log(userId)
         const deck = await Deck.findOne({ _id: id });
@@ -15,16 +15,16 @@ export const deckCheck = async (req, res, next) => {
             return res.status(404).json({ message: 'Deck not found' });
         }
         // Verificar si el mazo es público o pertenece al usuario actual
-        console.log(deck.isPublic, deck.userId, user._id, '¡¡!!')
+        // console.log(deck.isPublic, deck.userId, user._id, '¡¡!!')
         if (!deck.isPublic && (deck.userId !== user._id) ) {
             return res.status(403).json({ message: 'Acceso denegado: este mazo no es publico' });
         }
 
         req.deck = await deck.populate(['crypt._id', 'library._id']);
-        console.log('Deck found:', req.deck);
+        // console.log('Deck found:', req.deck);
         next();
     } catch (error) {
-        console.log('Error al obtener el ID del mazo', error);
+        // console.log('Error al obtener el ID del mazo', error);
         res.status(400).json({ error: error.message });
     }
 }
--- src/models/image.model.js
+++ src/models/image.model.js
@@ -19,6 +19,7 @@ const imageSchema = new Schema({
     },
     name: {
         type: String,
+        required: [true, 'El nombre de la imagen es obligatorio'],
     },
     image: {
         type: String,
